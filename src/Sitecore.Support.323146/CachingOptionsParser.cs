445e3310955b481623d4ffc9dba15cec09cba1c9
iro <iro@sitecore.net>
2019-04-25 11:59:01 +0300

    Initial

---
+++ src/Sitecore.Support.323146/CachingOptionsParser.cs
@@ -0,0 +1,42 @@
+﻿namespace Sitecore.Support.XA.Foundation.Presentation
+{
+  using Sitecore.Data.Items;
+  using Sitecore.Layouts;
+  using Sitecore.Mvc.Presentation;
+  using Sitecore.XA.Foundation.Presentation;
+  using Sitecore.XA.Foundation.Presentation.Extensions;
+
+  public class CachingOptionsParser : TokenDatasourceParser
+  {
+    protected override void AddRenderingItemProperties(Rendering rendering)
+    {
+      RenderingItem renderingItem = rendering.RenderingItem;
+      if (renderingItem != null)
+      {
+        if (!string.IsNullOrEmpty(rendering.DataSource))
+        {
+          ResolveDatasource(rendering);
+        }
+        AddCachingProperties(rendering, renderingItem);
+        AddRenderingItemProperty(rendering, "DataSource", renderingItem.DataSource);
+        AddRenderingItemProperty(rendering, "Placeholder", renderingItem.Placeholder);
+        AddRenderingItemProperty(rendering, "Parameters", renderingItem.Parameters);
+      }
+    }
+
+    protected virtual void AddCachingProperties(Rendering rendering, RenderingItem renderingItem)
+    {
+      bool flag = false;
+      string text = rendering.Parameters["Reset Caching Options"];
+      if (!string.IsNullOrEmpty(text))
+      {
+        flag = text.Equals("1");
+      }
+      if (!(rendering.Caching.Cacheable | flag))
+      {
+        RenderingCaching caching = renderingItem.Caching;
+        rendering.SetCachingOptions(caching);
+      }
+    }
+  }
+}
---
+++ src/Sitecore.Support.323146/InjectCustomDatasourceParser.cs
@@ -0,0 +1,16 @@
+﻿namespace Sitecore.Support.XA.Foundation.Presentation.Pipelines.Initialize
+{
+  using Sitecore.Mvc.Configuration;
+  using Sitecore.Mvc.Presentation;
+  using Sitecore.Pipelines;
+  using Sitecore.XA.Foundation.Presentation;
+  using System;
+
+  public class InjectCustomDatasourceParser
+  {
+    public virtual void Process(PipelineArgs args)
+    {
+      MvcSettings.RegisterObject((Func<XmlBasedRenderingParser>)(() => new Sitecore.Support.XA.Foundation.Presentation.CachingOptionsParser()));
+    }
+  }
+}
